2e9ebbdf602fb0372b1c175303f2bdcd285293cf
uwo <onno3940@gmail.com>
2016-04-04 11:56:15 -0400

    autocomplete example

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject om-tutorial "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [org.omcljs/om "1.0.0-alpha32"]
+                 [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "test"]])
